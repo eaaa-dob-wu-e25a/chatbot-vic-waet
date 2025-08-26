f759a2cc0829f49a72ddf19ded61dcef65c25f25
vicwaet <vicwaet@hotmail.com>
2025-08-26 11:05:44 +0200

    chatbot module
    i accidentally ran "npm i" twice

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express"; // Import express framework 
+const server = express();// Create an instance of express// Listen on port 3000
+
+//NOTE - Saving names in array upon submitting
+const names = [];
+// const ages = [];
+const messages = []; //NOTE - saves messages
+
+server.set("view engine", "ejs"); // Set EJS as the templating engine 
+
+server.use(express.urlencoded({extended:true})); // For parsing application/x-www-form-urlencoded
+server.use(express.static("style"));
+server.get("/", (req, res) => { // GET - render the index page
+  res.render("index", { name: "", age: "", error: "", names, messages, botReply:""}); // NOTE - sending all required params
+
+});
+
+
+
+//SECTION - ChatBot msg
+
+server.post("/chat", (req, res) => {
+  const userMessage = req.body.message; // Fetches messages from forminput
+  let botReply = "";
+
+  // Check if msg is empty
+  if (!userMessage || userMessage.trim() === "") {
+    botReply = "How can I help you?";
+  } else {
+    botReply = `Your request: "${userMessage}"`;
+  }
+
+  // Saving user and bot msg
+  messages.push({ sender: "User", text: userMessage });
+  messages.push({ sender: "Bot", text: botReply });
+
+  // Send data to ESJ template
+  res.render("index", { messages, botReply });
+})
+ 
+// Listen on port 3000
+const port = 3300;
+server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
