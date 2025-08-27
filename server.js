65ece343fd404a5f926c38417dac888e7ac46c49
vicwaet <vicwaet@hotmail.com>
2025-08-27 20:28:36 +0200

    `Refactor server.js, style files, and views/index.ejs for consistency and minor changes.`

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-import express from "express"; // Import express framework 
-const server = express();// Create an instance of express// Listen on port 3000
+import express from "express"; // Import express framework
+const server = express(); // Create an instance of express// Listen on port 3000
 
 //NOTE - Saving names in array upon submitting
 const names = [];
 // const ages = [];
 const messages = []; //NOTE - saves messages
 
-server.set("view engine", "ejs"); // Set EJS as the templating engine 
+server.set("view engine", "ejs"); // Set EJS as the templating engine
 
-server.use(express.urlencoded({extended:true})); // For parsing application/x-www-form-urlencoded
+server.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 server.use(express.static("style"));
-server.get("/", (req, res) => { // GET - render the index page
-  res.render("index", { name: "", age: "", error: "", names, messages, botReply:""}); // NOTE - sending all required params
+server.use(express.static("public"));
 
+server.get("/", (req, res) => {
+  // GET - render the index page
+  res.render("index", {
+    name: "",
+    age: "",
+    error: "",
+    names,
+    messages,
+    botReply: "",
+  }); // NOTE - sending all required params
+});
+
+server.get("/chat", (req, res) => {
+  // GET - render the chat page
+  res.render("chat", {
+    name: "",
+    age: "",
+    error: "",
+    names,
+    messages,
+    botReply: "",
+  });
 });
 
 //SECTION - ChatBot msg
-
 server.post("/chat", (req, res) => {
   const userMessage = req.body.message; // Fetches messages from forminput
   let botReply = "";
@@ -34,8 +54,10 @@ server.post("/chat", (req, res) => {
 
   // Send data to ESJ template
   res.render("index", { messages, botReply });
-})
- 
+});
+
 // Listen on port 3000
 const port = 3300;
-server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+server.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
